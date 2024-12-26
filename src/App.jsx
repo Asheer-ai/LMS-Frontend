4b726ca47188e1d96c43977ce7b96976b66461eb
Asheer-ai <asheerazam12@gmail.com>
2024-12-26 15:15:56 +0530

    Added 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './Components/Footer'
 import HomeLayout from './Layouts/HomeLayout'
 import HomePage from './Pages/HomePage'
 import AboutUs from './Pages/AboutUs'
+import NotFound from './Pages/NotFound'
 
 function App() {
   
@@ -13,6 +14,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/about' element={<AboutUs/>}></Route>
+
+        <Route path='*' element={<NotFound/>}></Route>
       </Routes>
       
     </>
