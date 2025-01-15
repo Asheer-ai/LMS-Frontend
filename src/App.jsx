d47747959d6decc2a42001bbfb9ddcb3bba99a65
Asheer-ai <asheerazam12@gmail.com>
2025-01-15 17:42:38 +0530

    created denied page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Signup from './Pages/Signup'
 import Login from './Pages/Login'
 import CourseList from './Pages/Course/CourseList'
 import Contact from './Pages/Contact'
+import Denied from './Pages/Denied'
+import CourseDescription from './Pages/Course/CourseDescription'
 
 function App() {
   
@@ -20,6 +22,8 @@ function App() {
         <Route path='/about' element={<AboutUs/>}></Route>
         <Route path='/courses' element={<CourseList/> }></Route>
         <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/denied' element={<Denied/>}></Route>
+        <Route path='/coursesdescription' element={<CourseDescription/> }></Route>
 
         <Route path='/signup' element={<Signup/>}></Route>
         <Route path='/login' element={<Login/> }></Route>
---
+++ src/Pages/Course/CourseDescription.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+function CourseDescription() {
+
+  const locator=useLocation()
+
+  return (
+    <div>CourseDescription</div>
+  )
+}
+
+export default CourseDescription
